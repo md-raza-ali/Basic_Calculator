2b68616588e0690f87dba12e3cfb59b20bd92e21
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-12-07 09:40:56 -0800

    dark theme

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// function input(x) {
-//   var userInput = document.getElementById("display");
-//   userInput.value = x;
-// }
+let theme = document.querySelector("#darkLight");
+let body = document.querySelector("body");
+let h1 = document.querySelector("h1");
+theme.addEventListener("click", function () {
+  //   body.style.background = "#212121";
+  if ((body.style.background !== "white")) {
+    body.style.background = "white";
+    h1.style.color = "black";
+    theme.setAttribute("class", "fa-solid fa-moon")
+    
+} else {
+    body.style.background = "#212121";
+    h1.style.color = "white";
 
+  }
+});
