d10d97d666e962b28661fd51e755c76a977f6ffd
Md-RazaAli <muhammadrazaa263@gmail.com>
2023-12-08 07:25:34 -0800

    fixed bugs

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let theme = document.querySelector("#darkLight");
 let body = document.querySelector("body");
 let h1 = document.querySelector("h1");
+let input = document.querySelector("input");
 theme.addEventListener("click", function () {
-  //   body.style.background = "#212121";
-  if ((body.style.background !== "white")) {
+  if (body.style.background !== "white") {
     body.style.background = "white";
     h1.style.color = "black";
-    theme.setAttribute("class", "fa-solid fa-moon")
-    
-} else {
+    input.style.color = "#212121";
+    theme.setAttribute("class", "fa-solid fa-moon");
+  } else {
     body.style.background = "#212121";
     h1.style.color = "white";
-
+    theme.setAttribute("class", "fa-solid fa-sun");
   }
 });
